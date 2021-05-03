70356bf3dbf3ab663895ea6d372530d45155d577
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-05-03 18:25:36 +0100

    added ABV & IBU filter functionality

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
 // variables
 const urlBase = "https://api.punkapi.com/v2/beers";
-const beersDiv = document.querySelector('.beers')
+const filterABV = document.getElementById('filterABV')
+let optionsABV = ""
+let optionsIBU = ""
 
-/* Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
-Render each beer name inside the div with the class of beers that currently exists in the HTML file. */
+// filters
+filterABV.addEventListener("change", e => {
+    const value = e.target.value
+
+    switch (value) {
+        case "all":
+            optionsABV = ""
+            break;
+/* abv_lt is a param from https://punkapi.com/documentation/v2 docs. 
+abv_lt=4.6 Returns all beers with ABV less than 4.6 */
+        case "weak":
+            optionsABV = "abv_lt=4.6"
+            break;
+        case "medium":
+// chose gt=4.5 and lt=6.5 because it'll return beers between 4.6% and 6.4%
+            optionsABV = "abv_gt=4.5&abv_lt=6.9"
+            break;
+        case "strong":
+            optionsABV = "abv_gt=6.8"
+            break;
+    }
+    getBeers()
+})
+
+filterIBU.addEventListener("change", e => {
+    const value = e.target.value
+
+    switch (value) {
+        case "all":
+            optionsIBU = ""
+            break;
+        case "weak":
+            optionsIBU = "ibu_lt=41"
+            break;
+        case "medium":
+            optionsIBU = "ibu_gt=40&ibu_lt=75"
+            break;
+        case "strong":
+            optionsIBU = "ibu_gt=74"
+            break;
+    }
+    getBeers()
+})
 
 async function getBeers() {
+    const url = `${urlBase}?${optionsABV}&${optionsIBU}`
+    console.log(url)
     // fetch
-    const beerPromise = await fetch(urlBase)
+    const beerPromise = await fetch(url)
     const beers = await beerPromise.json()
-    console.log(beers)
+    
+    // render data
+    const beersDiv = document.querySelector('.beers')
     let beerHtml = ""
+
     beers.forEach(beer => {
         beerHtml += `
             <div class="beer-wrapper card">
@@ -26,7 +74,7 @@ async function getBeers() {
                     <h2 class="beer-name">${beer.name}</h2>
                     <h3 class="beer-tagline">${beer.tagline}</h3>
                     <p class="beer-description">${beer.description}</p>
-                    <p class="beer-pairings">Pair with: ${beer.food_pairing.join(', ') } </p>   
+                    <p class="  ">Pair with: ${beer.food_pairing.join(', ') } </p>   
                 </div>
             </div>
         `
