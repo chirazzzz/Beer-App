5dc227684013675323bd4d78649c1a7b70956523
chirazzzz <chiragmehta.uk@gmail.com>
2022-03-24 17:19:02 +0000

    created README

---
+++ README.md
@@ -0,0 +1,66 @@
+# 🔍🍺 Punk API Beer App
+
+[Link to live deploy](https://beerfinder-api.netlify.app/)
+
+This is a beer finder site which I created by using [Punk API](https://punkapi.com/documentation/v2).
+I have recently graduated from the [School of Code](https://www.schoolofcode.co.uk/) however this site was created in 2021.
+
+## Table of contents
+
+- [Running Locally](#running-locally)
+- [Description](#description)
+- [Challenges](#challenges)
+- [Overview](#overview)
+  - [My Goals](#my-goals)
+  - [Screenshot](#screenshot)
+- [My Process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Running Locally
+
+Since this site is made with HTML, CSS and JavaScript it doesn't require any setup or installing NPM modules. Simply clone or fork the repo onto your local machine and open the index.html file using your web browser. It looks great in Chrome, Firefox and Safari!
+
+## Description
+
+This was my first taste of utilising an external API and really cemented some core principals about RESTful APIs. It was great way to practice with fetch, async/await and promises. I researched how to create card overlays in hover states. The additional information is displayed when users show interest but doesn't clutter the screen by default. I think this really adds to the UI of the site.
+
+## Challenges
+
+- The biggest issue was working with the API and testing it to see what information returned
+- Creating the pagination manually without any external libraries or NPM modules took time but I'm happy I got the logic working
+- Getting the beer overlay to fit perfectly over the card was difficult but using calc sorted it out!
+
+## Overview
+
+### My goals
+
+Users should be able to:
+
+- Filter Brewdog beers by Alcohol Vol(ABV) and Hoppiness(IBU)
+- Go through the filtered results by using the pagination buttons
+- Hover over any beer and read further information in the overlay
+
+### Screenshot
+
+![Screenshot of portfolio site](img/Punk%20API%20Beer%20App.png)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- ES6 features like Promises, Async/Await and Fetch
+
+### What I learned
+
+The main things I learned were fetching data from RESTful APIs, reading documentation which isn't always clear and practicing using ES6 features in JavaScript.
+
+## Author
+
+- LinkedIn - [@chiragmehtauk](https://www.linkedin.com/in/chiragmehtauk/)
+- Twitter - [@chirag_uk](https://twitter.com/chirag_uk)
--- main.js
+++ main.js
@@ -1,123 +1,126 @@
 // variables
-const urlBase = "https://api.punkapi.com/v2/beers?per_page=12"
-const beersDiv = document.querySelector('.beers')
-const filterABV = document.getElementById('filterABV')
-const filterIBU = document.getElementById('filterIBU')
-const pageNumber = document.getElementById('pageNumber')
-const prevPage = document.getElementById('prevPage')
-const nextPage = document.getElementById('nextPage')
-let optionsABV = "", optionsIBU = "", page = 1
-
+const urlBase = "https://api.punkapi.com/v2/beers?per_page=12";
+const beersDiv = document.querySelector(".beers");
+const filterABV = document.getElementById("filterABV");
+const filterIBU = document.getElementById("filterIBU");
+const pageNumber = document.getElementById("pageNumber");
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
+let optionsABV = "",
+  optionsIBU = "",
+  page = 1;
 
 // filters
-filterABV.addEventListener("change", e => {
-    const value = e.target.value
-    
-    switch (value) {
-        case "all":
-            optionsABV = ""
-            break;
-/* abv_lt is a param from https://punkapi.com/documentation/v2 docs. 
-abv_lt=4.6 Returns all beers with ABV less than 4.6 */
-        case "weak":
-            optionsABV = "&abv_lt=4.6"
-            break;
-        case "medium":
-// chose gt=4.5 and lt=6.5 because it'll return beers between 4.6% and 6.4%
-            optionsABV = "&abv_gt=4.5&abv_lt=6.9"
-            break;
-        case "strong":
-            optionsABV = "&abv_gt=6.8"
-            break;
-    }
+filterABV.addEventListener("change", (e) => {
+  const value = e.target.value;
 
-    page = 1
-    getBeers()
-})
+  switch (value) {
+    case "all":
+      optionsABV = "";
+      break;
+    /* abv_lt is a param from https://punkapi.com/documentation/v2 docs. 
+      abv_lt=4.6 Returns all beers with ABV less than 4.6 */
+    case "weak":
+      optionsABV = "&abv_lt=4.6";
+      break;
+    case "medium":
+      // chose gt=4.5 and lt=6.5 because it'll return beers between 4.6% and 6.8%
+      optionsABV = "&abv_gt=4.5&abv_lt=6.9";
+      break;
+    case "strong":
+      optionsABV = "&abv_gt=6.8";
+      break;
+  }
 
-filterIBU.addEventListener("change", e => {
-    const value = e.target.value
+  page = 1;
+  getBeers();
+});
 
-    switch (value) {
-        case "all":
-            optionsIBU = ""
-            break;
-        case "weak":
-            optionsIBU = "&ibu_lt=35"
-            break;
-        case "medium":
-            optionsIBU = "&ibu_gt=34&ibu_lt=75"
-            break;
-        case "strong":
-            optionsIBU = "&ibu_gt=74"
-            break;
-    }
+filterIBU.addEventListener("change", (e) => {
+  const value = e.target.value;
 
-    page = 1
-    getBeers()
-})
+  switch (value) {
+    case "all":
+      optionsIBU = "";
+      break;
+    case "weak":
+      optionsIBU = "&ibu_lt=35";
+      break;
+    case "medium":
+      optionsIBU = "&ibu_gt=34&ibu_lt=75";
+      break;
+    case "strong":
+      optionsIBU = "&ibu_gt=74";
+      break;
+  }
+
+  page = 1;
+  getBeers();
+});
 
 async function getBeers() {
-    const url = `${urlBase}&page=${page}${optionsABV}${optionsIBU}`
-    console.log(url)
-    // fetch
-    const beerPromise = await fetch(url)
-    const beers = await beerPromise.json()
+  const url = `${urlBase}&page=${page}${optionsABV}${optionsIBU}`;
+  console.log(url);
+  // fetch
+  const beerPromise = await fetch(url);
+  const beers = await beerPromise.json();
 
-    // pagination
-    pageNumber.textContent = page
+  // pagination
+  pageNumber.textContent = page;
 
-    if (page === 1) {
-        prevPage.disabled = true
-    } else {
-        prevPage.disabled = false
-    }
+  if (page === 1) {
+    prevPage.disabled = true;
+  } else {
+    prevPage.disabled = false;
+  }
 
-    if (beers.length < 12) {
-        nextPage.disabled = true
-    } else {
-        nextPage.disabled = false
-    }
-    
-    // render data
-    let beerHtml = ""
-    const genericBottle = 'https://images.pexels.com/photos/5659172/pexels-photo-5659172.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  if (beers.length < 12) {
+    nextPage.disabled = true;
+  } else {
+    nextPage.disabled = false;
+  }
 
+  // render data
+  let beerHtml = "";
+  const genericBottle =
+    "https://images.pexels.com/photos/5659172/pexels-photo-5659172.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
-    beers.forEach(beer => {
-        beerHtml += `
-            <div class="beer-wrapper card">
-                <div class="beer">
-                    <img class="beer-img" src="${beer.image_url || genericBottle}" alt="photo of ${beer.name}">
-                    <h3>${beer.name}</h3>
-                    <span class="beer-info">
-                        <span>ABV: ${beer.abv}%</span>
-                        <span>IBU: ${beer.ibu}</span>
-                    </span>
-                </div>
-                <div class="beer-content">
-                    <h2 class="beer-name">${beer.name}</h2>
-                    <h3 class="beer-tagline">${beer.tagline}</h3>
-                    <p class="beer-description">${beer.description}</p>
-                    <p class="  ">Pair with: ${beer.food_pairing.join(', ') } </p>   
-                </div>
-            </div>
-        `
-    });
+  beers.forEach((beer) => {
+    beerHtml += `
+      <div class="beer-wrapper card">
+        <div class="beer">
+          <img class="beer-img" src="${
+            beer.image_url || genericBottle
+          }" alt="photo of ${beer.name}">
+          <h3>${beer.name}</h3>
+          <span class="beer-info">
+            <span>ABV: ${beer.abv}%</span>
+            <span>IBU: ${beer.ibu}</span>
+          </span>
+        </div>
+        <div class="beer-content">
+          <h2 class="beer-name">${beer.name}</h2>
+          <h3 class="beer-tagline">${beer.tagline}</h3>
+          <p class="beer-description">${beer.description}</p>
+          <p class="  ">Pair with: ${beer.food_pairing.join(", ")} </p>   
+        </div>
+      </div>
+    `;
+  });
 
-    beersDiv.innerHTML = beerHtml
+  beersDiv.innerHTML = beerHtml;
 }
 
 // pagination
-prevPage.addEventListener('click', () => {
-    page--
-    getBeers()
-})
+prevPage.addEventListener("click", () => {
+  page--;
+  getBeers();
+});
 
-nextPage.addEventListener('click', () => {
-    page++
-    getBeers()
-})
+nextPage.addEventListener("click", () => {
+  page++;
+  getBeers();
+});
 
 // initial get
-getBeers()
+getBeers();
